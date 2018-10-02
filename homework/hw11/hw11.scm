6af619baae6402b856a2adf6a537e4c07ec87eda
Aijm <1907059378@qq.com>
2018-10-02 23:20:03 +0800

    finish hw11(partially),lab12

--- homework/hw11/hw11.scm
+++ homework/hw11/hw11.scm
@@ -1,13 +1,29 @@
 (define (find s predicate)
-  'YOUR-CODE-HERE
+  (cond ((null? s) #f)
+        ((eq? (predicate (car s)) #t) (car s))
+        (else (find (cdr-stream s) predicate)))
 )
 
 (define (scale-stream s k)
-  'YOUR-CODE-HERE
+  (cons-stream (* (car s) k) (scale-stream (cdr-stream s) k))
 )
 
 (define (has-cycle s)
   'YOUR-CODE-HERE
+ (define (in? lst s)  ;Check if stream s is in list lst
+  (cond
+   ((null? lst) #f)
+   ((eq? (car lst) s) #t)
+   (else (in? (cdr lst) s))))
+
+ (define (has-cycle-helper memo s)
+   (cond
+    ((null? s) #f)
+    ((in? memo s) #t)
+    (else (has-cycle-helper (cons s memo) (cdr-stream s)))))
+
+ (has-cycle-helper nil s)
+
 )
 (define (has-cycle-constant s)
   'YOUR-CODE-HERE
--- lab/lab12/lab12_extra.sql
+++ lab/lab12/lab12_extra.sql
@@ -2,36 +2,41 @@
 
 -- Q5
 CREATE TABLE greatstudents AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT this.date, this.color, this.pet, this.number, last.number 
+    FROM students AS this, fa17students AS last
+    WHERE this.date == last.date AND this.color == last.color AND this.pet == last.pet;
 
 -- Q6
 CREATE TABLE sevens AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT a.seven FROM students AS a, checkboxes AS b 
+    WHERE a.time == b.time AND a.number == 7 AND b.'7' == 'True';
 
 -- Q7
 CREATE TABLE fa17favnum AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT number, count(*) AS count FROM fa17students GROUP BY number ORDER BY count DESC LIMIT 1;
 
 
 CREATE TABLE fa17favpets AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT pet, count(*) AS count FROM fa17students GROUP BY pet ORDER BY count DESC LIMIT 10;
 
 
 CREATE TABLE sp18favpets AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT pet, count(*) AS count FROM students GROUP BY pet ORDER BY count DESC LIMIT 10;
 
 
 CREATE TABLE sp18dog AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT pet, count(*) AS count FROM students WHERE pet == 'dog' GROUP BY pet;
 
 
 CREATE TABLE sp18alldogs AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT pet, count(*) AS count FROM students WHERE pet LIKE '%dog%';
 
 
 CREATE TABLE obedienceimages AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT seven, denero, count(*) AS count FROM students
+    WHERE seven == '7' GROUP BY denero;
 
 -- Q8
 CREATE TABLE smallest_int_count AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT smallest, count(*) AS count FROM students
+    GROUP BY smallest;
